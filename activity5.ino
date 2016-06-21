68957514958a6a7948bb2929fd5e42da904e774b
Quinn McCabe <quinnmccabe@users.noreply.github.com>
2016-06-21 13:22:52 -0400

    more comments, clear lcd in setup

--- activity5.ino
+++ activity5.ino
@@ -10,32 +10,39 @@ Pololu3pi robot;
 OrangutanPushbuttons buttons;
 OrangutanLCD lcd;
 
-// change this value if the black line sensitivity is //too much or too little 
+// change this value if the black line sensitivity is too much or too little 
 const unsigned int THRESHOLD = 275;
- 
-bool IsWhite()
- {
-    unsigned int sensors[5];
-    robot.readLineSensors(sensors, IR_EMITTERS_ON);
-    return (sensors[0] < THRESHOLD || sensors[1] < THRESHOLD || sensors[2] < THRESHOLD || sensors[3] < THRESHOLD || sensors[4] < THRESHOLD);
- }
+
+// the IsWhite function checks the sensors to see if they are on top of
+// something white or black
+bool IsWhite() {
+  unsigned int sensors[5];
+  robot.readLineSensors(sensors, IR_EMITTERS_ON);
+  return (sensors[0] < THRESHOLD || sensors[1] < THRESHOLD || sensors[2] < THRESHOLD || sensors[3] < THRESHOLD || sensors[4] < THRESHOLD);
+}
+
 void setup() {
-    lcd.print("GO BRO!!!"");
-    robot.init(2000);
-    robot.calibrateLineSensors(IR_EMITTERS_ON);
-    delay(200);
-    buttons.waitForButtons(ALL_BUTTONS);
-    motors.setSpeeds(80, 80);
+  lcd.clear(); 
+  lcd.print("Sensors!");
+  
+  robot.init(2000);
+  robot.calibrateLineSensors(IR_EMITTERS_ON);
+  delay(200);
+  buttons.waitForPress(ALL_BUTTONS);
+  motors.setSpeeds(80, 80);
 }
 
 void loop() {
-    if (IsWhite()) {
-        motors.setSpeeds(20,20);
-    } else {
-        motors.setSpeeds(0, 55);  
+  // if the sensors are over something white, keep moving forward
+  if (IsWhite()) {
+    motors.setSpeeds(20,20);
+
+  // if the sensors are over somthing black, turn right
+  } else {
+    motors.setSpeeds(0, 55);  
         
-        // Change this value to turn more or less
-        delay(600);		  
-        return;
-    }
+    // Change this value to turn more or less
+    delay(600);		  
+    return;
+  }
 }
